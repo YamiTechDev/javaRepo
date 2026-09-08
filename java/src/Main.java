0d874d7ef85c0e94c304236e55b2eb8a0d1ce9a7
Тетлер Стас <stasselez@yandex.ru>
2026-09-08 16:08:04 +0300

    create branch 1.2.6 and Main.java

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+        byte varByte = 100;
+        Short varShort = 32700;
+        int varInt = 390000;
+        long varLong = 45000000;
+        Float varFloat = 4556.005000000f;
+        double varDouble = 323.15d;
+        Boolean varBoolean = true;
+        char varChar = 'a';
+           }
 }

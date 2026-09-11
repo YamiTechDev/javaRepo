5294cfb1a112fddbb5ccfde390b8f672abb9eabe
Тетлер Стас <stasselez@yandex.ru>
2026-09-11 19:54:15 +0300

    create code main CharExpression.java

--- java/src/CharExpression.java
+++ java/src/CharExpression.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
-public static char charExpression(int a){
-    char result;
-    return result = (char) ('a' + a);
+public class CharExpression{
+    public static char charExpression(int a){
+        char result;
+        return result = (char) ('a' + a);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число ");
+        int userEnter = scan.nextInt();
+        char userResult = charExpression(userEnter);
+        System.out.println("результат  " + userResult);
+
+    }
+
 }
 
-void main() {
-}
+
